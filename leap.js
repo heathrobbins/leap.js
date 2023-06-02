8ac40da555c7c7cdf4477e8db6cacac46f8fb046
Heath Robbins <heathrobbins89@gmail.com>
2023-06-02 12:46:03 -0400

    Create leap.js

---
+++ leap.js
@@ -0,0 +1,8 @@
+function findLeap(year){
+     if (year % 4 = 0 && year % 100 = 0){
+	console.log("It's a leap year!");
+     } else { 
+     console.log ("It's not a leap year!");
+     }
+}
+findLeap(2012);
